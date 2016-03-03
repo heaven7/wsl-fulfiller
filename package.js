1058db1ea29c2f2ff497469ff0657e0db39727c3
Christian Grlica <christian@heavenseven.net>
2016-03-03 12:05:38 +0100

    Release 0.0.4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'heaven7:wsl-fulfiller',
-  version: '0.0.3_2',
+  version: '0.0.4',
   summary: 'Search package',
   git: 'https://github.com/heaven7/wsl-fulfiller.git',
   documentation: 'README.md'
@@ -9,6 +9,9 @@ Package.describe({
 const both = ['client','server'],
     packages = [
         'heaven7:wsl-core@0.0.3_1',
+        'heaven7:wsl-i18n@0.0.3',
+        'heaven7:wsl-translations@0.0.3',
+        'templating',
         'easy:search@2.0.5',
         'easysearch:components@2.0.5',
         'easysearch:elasticsearch@2.0.3',
@@ -26,9 +29,10 @@ Package.onUse(function(api) {
         'lib/both/wsl-fulfiller.js'
     ], both)
 
-    api.addAssets([
+    api.addFiles([
         'lib/client/templates.html',
-        'lib/client/templates.js'
+        'lib/client/templates.js',
+        'lib/client/search.js'
     ], 'client')
 
 })
